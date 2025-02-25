9209a3107cedf13c9fd61f94d6de5269d523e01b
Ravi ThejaRavi Theja <kvrraviteja@gmail.com>
2025-02-25 06:40:22 -0800

    Initial Commit

---
+++ YumRecipes/YumRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  YumRecipesApp.swift
+//  YumRecipes
+//
+//  Created by Ravi Theja Karnatakam on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct YumRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YumRecipesTests/YumRecipesTests.swift
@@ -0,0 +1,17 @@
+//
+//  YumRecipesTests.swift
+//  YumRecipesTests
+//
+//  Created by Ravi Theja Karnatakam on 2/25/25.
+//
+
+import Testing
+@testable import YumRecipes
+
+struct YumRecipesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
